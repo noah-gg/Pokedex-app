8b0257d6f9a4e5f8ab68edd8277ba11475b8736e
noah-gg <99127702+noah-gg@users.noreply.github.com>
2022-03-14 18:11:50 +0000

    Create scripts.js
    If applied, this commit will create scripts.js base with favouriteFood variable

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+alert('Hello World');
+
+let favouriteFood = 'Oran Berry';
+document.write(favouriteFood);
+
+favouriteFood = 'Pokeblock';
+document.write(favouriteFood);
+
+favouriteFood = 'Curry';
+document.write(favouriteFood);
