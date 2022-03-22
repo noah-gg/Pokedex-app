d2531ac834bb0fea84cfe1b785850204cfb1a717
noah-gg <99127702+noah-gg@users.noreply.github.com>
2022-03-22 12:44:07 +0000

    Display Pokemon list using FOR loop and Conditions
    If applied, this commit will insert FOR loop with conditionals to display pokemon list.
    If pokemon height is greater than 1, website displays name and height with a label that says 'Wow, that's big!'. Else it will just display name and height only.

--- js/scripts.js
+++ js/scripts.js
@@ -145,21 +145,34 @@
 let pokemonList = [
     {
         name: 'Bulbasaur',
-        height: '0.7',
+        height: 0.7,
         type: ['Grass','Poison']   
     },
 
     {
         name: 'Ivysaur',
-        height: '1',
+        height: 1,
         type: ['Grass','Poison']   
     },
 
     {
         name: 'Venusaur',
-        height: '2',
+        height: 2,
         type: ['Grass','Poison']   
     }
 ]
 
-console.log(pokemonList);
+//  console.log(pokemonList);
+//  FOR loop for pokemon list
+for (let i = 0; i < pokemonList.length; i++) {
+    //  set variable for pokemon list     
+    let pokemon = pokemonList[i]
+
+    //  conditional for height = if taller than 1 THIS else THAT
+    //  document write pokemon name and height
+    if (pokemon.height > 1) {
+        document.write(pokemon.name + "\n" + '(height:' + "\n" + pokemon.height + ')' + ' - Wow, that\'s big!' + "<br>" + "<br>")   
+    } else {
+        document.write(pokemon.name + "\n" + '(height:' + "\n" + pokemon.height + ')' + "<br>" + "<br>")    
+    }
+}
