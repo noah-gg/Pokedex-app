1b3e172edde4a7595b019b59e7b60d0b35480d11
noah-gg <99127702+noah-gg@users.noreply.github.com>
2022-03-29 23:25:27 +0100

    Apply IIFE and update loop code
    If applied, this commit will apply IIFE and update loop code to cope with new changes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,48 @@
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 0.7,
-        type: ['Grass','Poison']   
-    },
+//  IIFE
+let pokemonRepository = ( function () {
 
-    {
-        name: 'Ivysaur',
-        height: 1,
-        type: ['Grass','Poison']   
-    },
+    //  Pokemon List
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 0.7,
+            type: ['Grass','Poison']   
+        },
 
-    {
-        name: 'Venusaur',
-        height: 2,
-        type: ['Grass','Poison']   
+        {
+            name: 'Ivysaur',
+            height: 1,
+            type: ['Grass','Poison']   
+        },
+
+        {
+            name: 'Venusaur',
+            height: 2,
+            type: ['Grass','Poison']   
+        }
+    ];
+
+    //  Assign -getAll- function to shows all pokemon
+    function getAll() {
+        return pokemonList;
     }
-]
 
+    //  Assign -add- function to add pokemon to end of array
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
 
-//  FOR loop for pokemon list
-//  for (let i = 0; i < pokemonList.length; i++) {   
-//     let pokemon = pokemonList[i]
-
-//     if (pokemon.height > 1) {
-//         document.write(pokemon.name + "\n" + '(height:' + "\n" + pokemon.height + ')' + ' - Wow, that\'s big!' + "<br>" + "<br>")   
-//     } else {
-//         document.write(pokemon.name + "\n" + '(height:' + "\n" + pokemon.height + ')' + "<br>" + "<br>")    
-//     }
-//  }
+    return {
+        getAll: getAll,
+        add: add
+    };
+})();
 
 //  forEach() external function
-function printPokemonInfo (pokemon) {
+pokemonRepository.getAll().forEach( function (pokemon) {
     if (pokemon.height > 1) {
         document.write(pokemon.name + "\n" + '(height:' + "\n" + pokemon.height + ')' + ' - Wow, that\'s big!' + "<br>" + "<br>");
     } else {
         document.write(pokemon.name + "\n" + '(height:' + "\n" + pokemon.height + ')' + "<br>" + "<br>")
     }
-}
-pokemonList.forEach(printPokemonInfo)
+}); 
